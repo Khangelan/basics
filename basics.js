32f53ffefd7ffa4e36c589ee1338919a0957331a
Khangelani Mhlakane <kmhlakane@gmail.com>
2025-10-31 10:13:37 +0200

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,12 @@
+// TODO: Create a variable called number1 and store any number in it 
+let number1 = 19
+// TODO: Create a variable called number2 and store a different number in it 
+let number2 = 20
+// TODO: Convert number1 to a string and store it in a new variable 
+const number1String = number1.toString();
+// TODO: Add number1 and number2 and store the result in a variable called sum 
+let sum = number1 + number2
+// TODO: Multiply number1 and number2 and store the result in a variable called product 
+let product = number1 * number2
+// TODO: Print all results to the console
+console.log(sum )
